74ed000a891292fa0208e86499c2a45a14143c07
Gopika T G <gopikatg379@gmail.com>
2025-11-07 12:34:05 +0530

    images modified in navbar.jsx

--- Frontend/freshCart/src/components/Navbar.jsx
+++ Frontend/freshCart/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { FaSearch, FaMapMarkerAlt, FaHeart, FaShoppingCart, FaTh, FaChevronDown, FaBars, FaTimes, FaSignOutAlt } from "react-icons/fa";
 import "../assets/css/Navbar.css";
-import img1 from '../assets/images/img1.jpg';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from "../context/AuthContext";
 import axios from 'axios';
